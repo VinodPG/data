4296a84925fe7c0da37752343c240cd5e3eb98ec
VinodPG <vinodpg99@gmail.com>
2022-03-03 19:56:11 +0530

    updated model

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-from products import db
+from config import db
 
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    sku = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    sku = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.name
